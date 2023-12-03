de1d37aaf3add22ca18a250bfe48795ca7fb64b6
Griffin Adams <griff4692@gmail.com>
2023-12-02 20:31:09 -0500

    Auto-update.

--- scripts/frost_inference.py
+++ scripts/frost_inference.py
@@ -273,7 +273,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--base_model', default='/nlp/projects/summarization/bhc_data_cleanup/mistral_weights/frost_instruct'
     )
-    parser.add_argument('--ckpt', default=3500)
+    parser.add_argument('--ckpt', default=4000)
 
     args = parser.parse_args()
 
