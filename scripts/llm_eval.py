1df698695924979bfecddf7be826001146d876f0
Griffin Adams <griff4692@gmail.com>
2023-12-06 14:51:15 -0500

    Auto-update.

--- scripts/llm_eval.py
+++ scripts/llm_eval.py
@@ -158,7 +158,7 @@ if __name__ == '__main__':
                 context, _ = top_k(rouge, pred_sent, full_note_sents_flat)
                 system = '<|system|>\nThe information in the SUMMARY sentence can be traced back to the SOURCE.\nDo you agree with this statement?\nAnswer with a single number from 1 (Strongly Disagree) to 5 (Strongly Agree).\n1 - Strongly Disagree\n2 - Disagree\n3 - Neutral\n4 - Agree\n5 - Strongly Agree</s>'
                 user = f'<|user|>\nSOURCE: {context}\nSUMMARY: {pred_sent}</s>'
-                assistant = '<|assistant|>\nSCORE: '
+                assistant = '<|assistant|>\nAGREEMENT SCORE: '
                 prompt = f'{system}\n{user}\n{assistant}'
 
                 output = run_prompt(prompt, model, tokenizer)
